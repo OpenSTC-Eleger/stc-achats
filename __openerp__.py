94b27daa652f9e931d939c54202ac9446ec5c928
plancher bruno <bruno.plancher@gmail.com>
2012-12-20 16:42:51 +0100

    premiere implementation des engagements et demande de prix avec bon de commande

--- __openerp__.py
+++ __openerp__.py
@@ -6,13 +6,13 @@
 ##############################################################################
 
 {
-    "name": "open_achat_stock",
+    "name": "openstc_achat_stock",
     "version": "0.1",
     "depends": ["purchase", "product", "stock", "openstc", "account_budget"],
     "author": "PYF & BP",
     "category": "Category",
     "description": """
-    Module Open ACHAT - STOCKS pour collecticités territoriales.
+    Module OpenSTC ACHAT - STOCKS pour collecticités territoriales.
     Gestion des fournitures par service de Collectivité (ST, écoles, etc...) ainsi que du process de réapprovisionnement.
     Fonctionnalités principales : 
         - Génération de bons de commandes
